c5665ef2f45c2916814949d21c811b70f20d121b
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-06 17:53:27 +0000

    Add exercise 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Description
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+			continue;
+
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
