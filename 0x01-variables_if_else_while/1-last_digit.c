d0cd3e1cd16349abf25bbe7e52c48f1674fde242
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-06 17:07:49 +0000

    Hotfix exercise 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
-		printf("and is and is greater than 5\n");
+		printf("and is greater than 5\n");
 	if (n % 10 == 0)
 		printf("and is 0\n");
 	if (n % 10 < 6 && n % 10 != 0)
