fde0fcd2d1dcaa2f1d2be4e8d39c31186724c9c1
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-06 18:47:28 +0000

    Add exercise 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Description
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
