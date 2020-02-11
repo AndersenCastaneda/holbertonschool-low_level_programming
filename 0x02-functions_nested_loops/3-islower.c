5a6fedd5ca6ee4c8bcfd4e031e5545e5c916fc19
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 07:34:35 +0000

    Add comment exercise 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 #include <ctype.h>
 
+/**
+ * _islower - chechk for lowercase character
+ * @c: the character to check.
+ * Return: 1 if c is lowercase, ) otherwise
+ */
+
 int _islower(int c)
 {
-	if (islower(c))
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
