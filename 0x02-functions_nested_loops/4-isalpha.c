4ee3eb51216c030d48f033e8a92e5700288494c5
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 07:38:47 +0000

    Add comment exercise 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 #include <ctype.h>
 
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
 int _isalpha(int c)
 {
-	if (isalpha(c))
-		return (1);
-	else
-		return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
