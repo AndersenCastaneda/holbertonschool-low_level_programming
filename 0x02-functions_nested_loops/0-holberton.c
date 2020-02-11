a1400076993f9355cd07b5375140897213fec5f6
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 07:11:43 +0000

    Hotfix exercise 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
 
 /*
- * main - Principal
- *
- * Return: 0
+ * main - print Holberton
+ * Return: Always 0
  */
 
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	char ch[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(ch[i]);
+	}
+
 	_putchar('\n');
 	return (0);
 }
