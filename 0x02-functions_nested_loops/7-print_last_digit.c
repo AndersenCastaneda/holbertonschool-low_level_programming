77c515d840b80673ca1f14728f4ee1ed804523be
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 08:03:40 +0000

    Add comment exercise 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 
-int print_last_digit(int i)
+/**
+ * print_last_digit - print the last digit.
+ * @n: number.
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
 {
 	int num;
 
-	if (i < 0)
-		i = i * (-1);
+	if (n < 0)
+		n = n * (-1);
 
-	num = i % 10;
+	num = n % 10;
 
 	if (num < 0)
 		num = num * (-1);
