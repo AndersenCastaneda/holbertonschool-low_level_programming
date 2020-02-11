6994d2a1f36ce54c22ab82c5f4fd6b427233d6b6
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 21:30:14 +0000

    Hotfix 2.1 exercise 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,27 +12,14 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
-	{
-		if (a > c)
-			largest = a;
-		else
-			largest = c;
-	}
-	else if (b > a)
-	{
-		if (b > c)
-			largest = b;
-		else
-			largest = c;
-	}
-	else if (c > a)
-	{
-		if (c > b)
-			largest = c;
-		else
-			largest = b;
-	}
+	if (a > b && a > c)
+		largest = a;
+	else if (a > b && a < c)
+		largest = c;
+	else if (a < b && b > c)
+		largest = b;
+	else if (a < b && b < c)
+		largest = c;
 
 	return (largest);
 }
