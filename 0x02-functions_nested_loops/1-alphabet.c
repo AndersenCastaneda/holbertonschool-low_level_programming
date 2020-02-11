29261402b7aaf1e677a5a539e84fed67d084ce0c
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 07:27:09 +0000

    Add comment exercise 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 
-/*
- * print_alphabet -
+/**
+ * print_alphabet - Prints the alphabet
+ *
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
 	char ch;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
