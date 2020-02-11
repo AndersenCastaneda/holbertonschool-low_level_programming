3c8242f150f4be26c37c458ee11e059e0130495e
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 08:17:06 +0000

    Add comment exercise 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * print_to_98 - print natural numbers from n to 98
+ * @n: print from this number to 98
+ * Return: Always 0.
+ */
+
 void print_to_98(int n)
 {
 	int i;
