be4ef259529e0fcf5a6825a03cebbfc65fc7879e
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-11 20:02:06 +0000

    Add advanced exercise 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the even-valued fibonacci terms
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	long int j = 0, k = 1, sum = 0, tmp, evn = 0;
+	long int lim = 4000000;
+
+	while (sum < lim)
+	{
+		sum = j + k;
+		if (sum % 2 == 0)
+			evn += sum;
+
+		tmp = k;
+		k = sum;
+		j = tmp;
+	}
+	printf("%li\n", evn);
+	return (0);
+}
