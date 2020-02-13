4dd47eb2ef7503b020494abf10fe8cab13c88db2
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-13 22:45:07 +0000

    Fix advanced exercise 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,20 @@
 
 void print_number(int n)
 {
+	unsigned int i;
+
 	if (n < 0)
 	{
-		n = -n;
+		i = -n;
 		_putchar('-');
 	}
+	else
+	{
+		i = n;
+	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (i / 10)
+		print_number(i / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((i % 10) + '0');
 }
