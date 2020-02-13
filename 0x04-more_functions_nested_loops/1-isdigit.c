1ee0cf2c410be1f4ffeed850ffaa87d392355880
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-13 19:16:53 +0000

    Hotfix 2.0 exercise 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - check if c is a number
- *@c: variable to check
- *Return: 1 if c is int, 0 otherwise
+ * @c: variable to check
+ * Return: 1 if c is int, 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c < 57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
