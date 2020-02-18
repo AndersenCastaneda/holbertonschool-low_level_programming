c211106150b4602af4ce4b4b55710acf6dbe3a6c
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-18 17:26:40 +0000

    Hotfix 2.1 exercise 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	i = 0;
-	do {
+	for (i = 0; src[i] != '\0'; i++)
 		*(dest + i) = *(src + i);
-		i++;
-	} while (src[i] != '\0');
 
+	*(dest + i) = '\0';
 	return (dest);
 }
