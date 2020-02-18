4ae9ea3ca29dabf931b28602053c151feaeb3953
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-18 16:06:23 +0000

    Add exercise 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - print n elements of an array
+ * @a: input (array)
+ * @n: input (quantity of prints)
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", *a[i]);
+		if (i != n - 1)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
