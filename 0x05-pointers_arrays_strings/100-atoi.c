53a2dff254541ab307aac029029bec12edeb6293
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-18 23:50:46 +0000

    Hotfix 2.4 advanced exercise 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,14 @@
 
 int _atoi(char *s)
 {
-	int i, j, k;
+	int i, j;
+	unsigned int k;
+	signed int l;
 
 	i = 0;
 	j = 0;
 	k = 0;
-
+	l = 0;
 	while ((s[i] < '1' || s[i] > '9') && s[i] != '\0')
 	{
 		if (s[i] == '-')
@@ -21,17 +23,25 @@ int _atoi(char *s)
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		k = (k * 10) + (s[i] - '0');
-		i++;
-	}
-
 	if (j % 2 != 0)
-		k = -k;
+	{
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			k = (k * 10) + (s[i] - '0');
+			i++;
+		}
+		return (k * (-1));
+	}
+	else
+	{
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			l = (l * 10) + (s[i] - '0');
+			i++;
+		}
+		return (l);
+	}
 
 	if (i == 0)
 		return (0);
-
-	return (k);
 }
