e7d26b1c5e5c0edc09bb83bc00c044c4fb60c4b4
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-18 05:37:03 +0000

    Hotfix exercise 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,22 @@ void puts_half(char *str)
 	}
 
 	if (i % 2 != 0)
-		j = (i - 1) / 2;
-	else
-		j = i / 2;
-
-	while (j <= i - 1)
 	{
-		_putchar(str[j]);
-		j++;
+		j = (i - 1) / 2;
+		while (j + 1 <= i - 1)
+		{
+			_putchar(str[j + 1]);
+			j++;
+		}
+	}
+	else
+	{
+		j = i / 2;
+		while (j <= i - 1)
+		{
+			_putchar(str[j]);
+			j++;
+		}
 	}
 	_putchar('\n');
 }
