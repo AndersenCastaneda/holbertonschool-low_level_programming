e02411caf0ae0115a3f5fd114efdddf68e02c122
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-20 17:13:20 +0000

    Add exercise 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+/**
+ * cap_string - capitalize all words of a string
+ * @c: input
+ * Return: char
+ */
+
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			if (i == 0 ||
+			    (i > 0 && c[i - 1] == ' ') ||
+			    (i > 0 && c[i - 1] == '.') ||
+			    (i > 0 && c[i - 1] == '\n') ||
+			    (i > 0 && c[i - 1] == '\t'))
+				c[i] = c[i] - 32;
+		}
+	}
+	return (c);
+}
