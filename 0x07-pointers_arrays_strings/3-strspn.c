ee152cd7e668a384a4b0270ebcf3459be76e4925
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-24 19:11:00 +0000

    Hotfix exercise 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,14 +7,16 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, count = 0;
+	unsigned int i, j = 0, count = 0;
 
 	for (i = 0; s[i] != ','; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				count++;
+			}
 		}
 	}
 	return (count);
