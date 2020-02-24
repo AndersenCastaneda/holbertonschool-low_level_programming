560d1eb4f8aa07e79e64943b353b94c4005fe51a
AndersenCastaneda <andersenpiano@gmail.com>
2020-02-24 17:48:39 +0000

    Add exercise 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+/**
+ * _strchr - locate a character in a string
+ * @s: input
+ * @c: input
+ * Return: char (pointer) or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p = NULL;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			p = &(*(s + i));
+			break;
+		}
+	}
+	return (p);
+}
