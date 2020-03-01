aa2d064d238edad5d67ce48242ff91bd36b25d80
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-01 02:45:53 -0500

    Add advanced exercise

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of change (coins)
+ * @argc: arguments count
+ * @argv: argc size
+ * Return: 0 (Success) or 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, num, result = 0;
+	int cents[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < 5 && num >= 0; i++)
+	{
+		while (num >= cents[i])
+		{
+			result++;
+			num -= cents[i];
+		}
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
