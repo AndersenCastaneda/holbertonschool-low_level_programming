6a9b79aba631e298ca3810f8035960503344d098
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-09 19:16:37 +0000

    Hotfix exercise 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include "dog.h"
+
 /**
  * init_dog - initialize dog structure
  * @d: dog pointer
@@ -10,7 +12,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return;
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
