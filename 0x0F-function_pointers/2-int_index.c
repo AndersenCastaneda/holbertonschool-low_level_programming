1f6779d46790ba3ec0f44dbfb0c63addf8657a50
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-11 18:02:33 +0000

    Fix exercise 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,15 +5,12 @@
  * @cmp: pointer to compare function
  * Return: integer
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp)
-
-		if (size <= 0)
-			return (-1);
+	if (!array || !cmp || size <= 0)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
