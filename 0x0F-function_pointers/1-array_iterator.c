452b6df204f60d3c485d6f0ca845be041349a200
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-11 17:40:38 +0000

    Hotfix 2.0 exercise 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,15 +6,15 @@
  * @array: array
  * @size: array's length
  * @action: pointer to action function
+ * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (!array || !action)
+	if ((!array || !action) && size > 0)
 		return;
 
 	for (i = 0; i < size; i++)
-		(*action)(array[i]);
+		action(array[i]);
 }
