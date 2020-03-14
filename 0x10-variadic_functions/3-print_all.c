da29fe94503cdbe9bc2ade3af4bd50a3ff9a3e5b
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-14 23:16:16 +0000

    Hotfix exercise 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,8 +8,8 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i;
-	char *str, *separator = "";
+	int i = 0;
+	char *str;
 
 	va_start(args, format);
 
@@ -17,29 +17,28 @@ void print_all(const char * const format, ...)
 	{
 		while (format[i])
 		{
-			switch (format[i])
+			switch (format[i++])
 			{
-				case 'c':
-					printf("%s%c", separator, va_arg(args, int));
-					break;
-				case 'i':
-					printf("%s%d", separator, va_arg(args, int));
-					break;
-				case 'f':
-					printf("%s%f", separator, va_arg(args, double));
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", separator, str);
-					break;
-				default:
-					i++;
-					continue;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				continue;
 			}
-			separator = ", ";
-			i++;
+			if (format[i])
+				printf(", ");
 		}
 	}
 
