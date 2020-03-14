00ab86e6586317681c4a8758e1a7f5d5effb63fc
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-14 23:05:56 +0000

    Hotfix exercise 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,7 +5,7 @@
  * @format: list of types of arguments passed to the function
  * Return: void
  */
-void print_all(const char *const format, ...)
+void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i;
@@ -19,21 +19,24 @@ void print_all(const char *const format, ...)
 		{
 			switch (format[i])
 			{
-			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				printf("%s%s", separator, str);
-				break;
-			default:
-				break;
+				case 'c':
+					printf("%s%c", separator, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
 			separator = ", ";
 			i++;
