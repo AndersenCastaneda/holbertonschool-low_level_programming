58fd61d9d9bd6c1f3530d67babceaf8218f69b37
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-14 23:44:49 +0000

    Hotfix exercise 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,20 +7,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
+	va_list arg;
 	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(args, n);
+	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, int);
+		sum += va_arg(arg, int);
 	}
 
-	va_end(args);
+	va_end(arg);
 	return (sum);
 }
