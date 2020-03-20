f6d702d179b1068478cf0a8020945af1e6da87d4
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-20 03:20:08 +0000

    Hotfix exercise 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,8 +4,7 @@
  * add_node_end - add a new node at the end of a linked list
  * @head: double pointer to the struct list_t
  * @str: string to put in the new node
- *
- * Return: address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
