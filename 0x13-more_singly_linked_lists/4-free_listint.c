95349e1c5ecc74e9c1398bbf4ef0e6d9bcf327ea
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-24 18:16:18 +0000

    Hotfix betty exercise 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * free_listint - frees a listint_t list
+ * @head: head linked list (listint_t)
+ * Return: void
+ */
 void free_listint(listint_t *head)
 {
 	while (head)
