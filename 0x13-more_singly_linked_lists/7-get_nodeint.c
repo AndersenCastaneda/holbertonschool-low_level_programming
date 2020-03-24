6b8a4bda88d3f23675640e5edd268b76d82fa896
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-24 21:01:12 +0000

    Hotfix betty exercise 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,17 +3,18 @@
 /**
  * get_nodeint_at_index -  returns the nth node of a listint_t linked list
  * @head: head linked list
- * @index:
- * Return:
+ * @index: position to get in the linked list
+ * Return: node in index position
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *temp = head;
+	listint_t *temp = malloc(sizeof(listint_t));
 
 	if (!head)
 		return (NULL);
 
+	temp = head;
 	for (i = 0; temp->next && i < index; i++)
 		temp = temp->next;
 
