c18c450d7dd3b2fc2a484c42e7d9c9b54a4fda3e
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-24 19:26:58 +0000

    Hoftix betty exercise 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
  * pop_listint - deletes the head node of a listint_t
  * linked list, and returns the head node’s data (n)
  * @head: head of the linked list
- * return (*head)->n, if head is null return 0
+ * Return: (*head)->n, if head is null return 0
  */
 int pop_listint(listint_t **head)
 {
