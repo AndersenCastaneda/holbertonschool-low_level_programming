c9418dc71c7d497057c3a6352869eccf0dd1ac5e
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-24 18:51:14 +0000

    Hotfix 2.2 exercise 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,7 +7,7 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (!*head)
+	if (!*head || !head)
 		return;
 
 	while (*head)
@@ -15,4 +15,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = (*head)->next;
 	}
+	free(*head);
 }
