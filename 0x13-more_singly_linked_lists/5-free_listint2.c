757e479991bc0d1673e4bc4050fc9c8da1380bd5
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-24 18:38:52 +0000

    Add exercise 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: head of the linked list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	while (*head)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+}
