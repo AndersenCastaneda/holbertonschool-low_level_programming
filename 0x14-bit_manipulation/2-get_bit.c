2c7c9c1f6442d7f0ad115a2d92d2ddcc44469701
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-26 23:52:16 +0000

    Add exercise 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,8 @@
 /**
-*get_bit - returns the value of a bit at a given index
-*@n: number to convert
-*@index: index of the bit
-*Return: the value of the bit at index or -1 if an error occured
+* get_bit - returns the value of a bit at a given index
+* @n: number to convert
+* @index: index of the bit
+* Return: the value of the bit at index or -1 if an error occured
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
