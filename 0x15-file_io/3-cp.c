2187a6a75425e893fcf2445520c72a3ad3133a2c
AndersenCastaneda <andersenpiano@gmail.com>
2020-04-01 04:30:03 +0000

    Add exercise 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int _file1, _file2, _read = 1024, count = 0;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
+	_file1 = open(argv[1], O_RDONLY);
+	if (_file1 == -1)
+		dprintf(STDERR_FILENO, FILE_READ_ERROR, argv[1]), exit(98);
+
+	_file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (_file2 == -1)
+		dprintf(STDERR_FILENO, FILE_WRITE_ERROR, argv[2]), exit(99);
+
+	while (_read == BUFFER_SIZE)
+	{
+		_read = read(_file1, buffer, BUFFER_SIZE);
+		if (_read == -1)
+			dprintf(STDERR_FILENO, FILE_READ_ERROR, argv[1]), exit(98);
+
+		count = write(_file2, buffer, _read);
+		if (count == -1)
+			dprintf(STDERR_FILENO, FILE_WRITE_ERROR, argv[2]), exit(99);
+	}
+	if (close(_file1) == -1)
+		dprintf(STDERR_FILENO, FILE_CLOSE_ERROR, _file1), exit(100);
+
+	if (close(_file2) == -1)
+		dprintf(STDERR_FILENO, FILE_CLOSE_ERROR, _file2), exit(100);
+	return (0);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,12 +1,19 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdarg.h>
+
+#define USAGE "Usage: cp file_from file_to\n"
+#define FILE_READ_ERROR "Error: Can't read from file %s\n"
+#define FILE_WRITE_ERROR "Error: Can't write to %s\n"
+#define FILE_CLOSE_ERROR "Error: Can't close fd  %d\n"
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
