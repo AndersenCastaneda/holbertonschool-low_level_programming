eb736feacb92419c7e8f4d5c4283f1d42eb4b550
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-03 22:56:33 +0000

    Fix check case 6

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,52 +1,44 @@
 #include "lists.h"
 
 /**
- * last_index - get the last index of a dlistint_t linked list
- * @head: dlistint_t linked list head;
- * Return: Last index value
- */
-unsigned int last_index(dlistint_t *head)
-{
-	unsigned int len = 0;
-
-	for (len = 0; head; len++)
-		head = head->next;
-
-	return (len - 1);
-}
-
-/**
- * delete_dnodeint_at_index - Deletes the node at index index of a
+ * delete_dnodeint_at_index - Deletes the node at index of a
  * dlistint_t linked list
- * @head: dlistint_t linked list
+ * @head: dlistint_t linked list head
  * @index: Index to delete
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *h = *head;
-	unsigned int i;
 
-	if (!head || index > last_index(h))
-		return (-1);
-
-	if (index == 0)
+	if (index == 0 && h)
 	{
-		*head = h->next;
 		if (h->next)
+		{
 			h->next->prev = NULL;
-		h->next = NULL;
+			*head = h->next;
+		}
+		else
+			*head = NULL;
+
 		free(h);
 		return (1);
 	}
 
-	for (i = 0; i < index; i++)
+	while (h)
+	{
+		if (index == 0)
+		{
+			h->prev->next = h->next;
+			if (h->next)
+				h->next->prev = h->prev;
+			free(h);
+			return (1);
+		}
+
 		h = h->next;
+		index--;
+	}
 
-	h->prev->next = h->next;
-	if (h->next)
-		h->next->prev = h->prev;
-
-	free(h);
-	return (1);
+	return (-1);
 }
