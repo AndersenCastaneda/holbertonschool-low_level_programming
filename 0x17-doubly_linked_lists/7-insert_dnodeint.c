606b752d7eca29684399ac1a56d2fccc60ec9b94
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-03 20:12:53 +0000

    Fix check case 6 and 11

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,24 +16,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (head && head->prev)
-		head = head->prev;
-
-	for (i = 0; head && i <= idx; i++)
+	for (i = 0; head && i < idx; i++)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
 			if (!head->next)
 				return (add_dnodeint_end(h, n));
 
-			new->next = head;
-			new->prev = head->prev;
-			head->prev->next = new;
-			head->prev = new;
+			new->next = head->next;
+			new->prev = head;
+			head->next->prev = new;
+			head->next = new;
 			return (new);
 		}
 		else
