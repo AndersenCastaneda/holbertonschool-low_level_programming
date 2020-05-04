b001d76e818ae84dbe966f7d9d2b853161f20f87
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-04 22:49:25 +0000

    update link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/AndersenCastaneda/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libwinjackpot.so
-export LD_PRELOAD=./libwinjackpot.so
+wget https://github.com/AndersenCastaneda/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=./libwin.so
