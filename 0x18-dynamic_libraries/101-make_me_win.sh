b8ff668eef29648db5963d258dc7b5ad93ae8399
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-04 22:42:51 +0000

    Check cases 4-6

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/AndersenCastaneda/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libwinjackpot.so
-export LD_PRELOAD=/tmp/libwinjackpot.so
+wget https://github.com/AndersenCastaneda/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libwinjackpot.so
+export LD_PRELOAD=./libwinjackpot.so
