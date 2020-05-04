6912431819685d5b2e7a9758dfa56b36f2646107
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-04 22:57:05 +0000

    Check cases 4 and 6

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/AndersenCastaneda/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=./libwin.so
+wget https://github.com/AndersenCastaneda/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libMagicWin.so
+export LD_PRELOAD=./libMagicWin.so
